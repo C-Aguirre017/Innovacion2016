99b5f9252c51d6d469433ac9072cea490ab64951
Carlos Aguirre <c.aguirre017@gmail.com>
2016-06-08 01:34:31 -0400

    Fix Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ puts "[+] Creating Habilities"
 Ability.create!(category: "Carrera",
                  name:"Ingeniería")
 Ability.create!(category: "Carrera",
-                 name:"Salud"),
+                 name:"Salud")
 Ability.create!(category: "Carrera",
                  name:"Leyes")
 Ability.create!(category: "Carrera",
